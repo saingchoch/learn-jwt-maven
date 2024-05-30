13d5b4d488148c31bed159b03e701fae86322d37
Saing Choch <saing.choch@princebank.com.kh>
2024-05-30 14:08:36 +0700

    Innitial commit

---
+++ src/main/java/com/mycompany/learn_jwt_mv/LearnJwtMvApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.learn_jwt_mv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJwtMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJwtMvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/learn_jwt_mv/LearnJwtMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.learn_jwt_mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJwtMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
